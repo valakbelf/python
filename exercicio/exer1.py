8aaa45af26f155c9c612c82f56e439b82a56c209
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-02-29 13:53:46 +0000

    exer1

--- exercicio/exer1.py
+++ exercicio/exer1.py
@@ -1,1 +1,19 @@
+idade=25
+nome='yuri'
+altura=1.70
+pessoa=["yuri","25","1.70"]
+ano_nascimento=2006
+salario=40000
+temperatura=("15ºc","35ºc","18ºc")
+aprovado = True
+reprovado = False
+horas_trabalhadas=40
+nota1 = 75
+
+print(idade)
+print(nome)
+print(altura)
+print(pessoa)
+
+
 
